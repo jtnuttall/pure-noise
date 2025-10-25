faba8747c540ae6c444885be44a807b648a6c65f
Jeremy Nuttall <jeremy@jeremy-nuttall.com>
2025-10-25 01:12:25 -0500

    perf: branchless opensimplex, 8-30% speedup

--- src/Numeric/Noise/OpenSimplex.hs
+++ src/Numeric/Noise/OpenSimplex.hs
@@ -9,6 +9,7 @@ module Numeric.Noise.OpenSimplex (
   noise2Base,
 ) where
 
+import Data.Bool (bool)
 import Numeric.Noise.Internal
 import Numeric.Noise.Internal.Math
 
@@ -35,36 +36,31 @@ noise2Base !seed !xo !yo =
       !j = fy * primeY
 
       !a = 0.5 - x0 * x0 - y0 * y0
-      n0 = attenuate a i j x0 y0
+      n0 = attenuate a seed i j x0 y0
 
-      {-# INLINE n1 #-}
-      n1
-        | y0 > x0 =
-            let x1 = x0 + g2
-                y1 = y0 + (g2 - 1)
-                j1 = j + primeY
-                b = 0.5 - x1 * x1 - y1 * y1
-             in attenuate b i j1 x1 y1
-        | otherwise =
-            let x1 = x0 + (g2 - 1)
-                i1 = i + primeX
-                y1 = y0 + g2
-                b = 0.5 - x1 * x1 - y1 * y1
-             in attenuate b i1 j x1 y1
+      n1 =
+        let cond = bool 0 1 (y0 > x0)
+            x1 = (x0 + g2 - 1) + fromIntegral cond
+            y1 = (y0 + g2) - fromIntegral cond
+            i1 = i + (1 - cond) * primeX
+            j1 = j + cond * primeY
+            b = 0.5 - x1 * x1 - y1 * y1
+         in attenuate b seed i1 j1 x1 y1
 
       !n2 =
         let g2t = 1 - 2 * g2
             c = 2 * g2t * (1 / g2 - 2) * t + (-2 * g2t * g2t + a)
             x2 = x0 + (2 * g2 - 1)
             y2 = y0 + (2 * g2 - 1)
-         in attenuate c (i + primeX) (j + primeY) x2 y2
+         in attenuate c seed (i + primeX) (j + primeY) x2 y2
    in normalize $ n0 + n1 + n2
- where
-  attenuate :: (RealFrac a) => a -> Hash -> Hash -> a -> a -> a
-  attenuate !v i j x y
-    | v <= 0 = 0
-    | otherwise = (v * v) * (v * v) * gradCoord2 seed i j x y
 {-# INLINE noise2Base #-}
+
+attenuate :: (RealFrac a) => a -> Seed -> Hash -> Hash -> a -> a -> a
+attenuate !vi !seed !i !j !x !y =
+  let !v = max 0 vi
+   in (v * v) * (v * v) * gradCoord2 seed i j x y
+{-# INLINE attenuate #-}
 
 normalize :: (RealFrac a) => a -> a
 normalize = (99.83685446303647 *)
