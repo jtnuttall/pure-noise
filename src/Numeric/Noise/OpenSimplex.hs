86e5c3a36da953dc8a7ac580ba19a02141f15a86
Jeremy Nuttall <jeremy@jeremy-nuttall.com>
2025-10-24 21:28:33 -0500

    refactor: improve opensimplex readability

--- src/Numeric/Noise/OpenSimplex.hs
+++ src/Numeric/Noise/OpenSimplex.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE Strict #-}
-
 -- |
 -- Maintainer: Jeremy Nuttall <jeremy@jeremy-nuttall.com>
 -- Stability: experimental
@@ -20,67 +18,54 @@ noise2 = Noise2 noise2Base
 
 noise2Base :: (RealFrac a) => Seed -> a -> a -> a
 noise2Base seed xo yo =
-  let f2 = 0.5 * (sqrt3 - 1)
-      to = (xo + yo) * f2
-      x = xo + to
-      y = yo + to
+  let !f2 = 0.5 * (sqrt3 - 1)
+      !to = (xo + yo) * f2
+      !x = xo + to
+      !y = yo + to
 
-      fx = floor x
-      fy = floor y
-      xi = x - fromIntegral fx
-      yi = y - fromIntegral fy
+      !fx = floor x
+      !fy = floor y
+      !xi = x - fromIntegral fx
+      !yi = y - fromIntegral fy
 
-      t = (xi + yi) * g2
-      x0 = xi - t
-      y0 = yi - t
+      !t = (xi + yi) * g2
+      !x0 = xi - t
+      !y0 = yi - t
       i = fx * primeX
       j = fy * primeY
 
-      a = 0.5 - x0 * x0 - y0 * y0
-      n0
-        | a <= 0 = 0
-        | otherwise =
-            (a * a)
-              * (a * a)
-              * gradCoord2 seed i j x0 y0
+      !a = 0.5 - x0 * x0 - y0 * y0
+      !n0 = attenuate a i j x0 y0
 
-      n1
+      !n1
         | y0 > x0 =
-            let ~x1 = x0 + g2
-                ~i1 = i
-                ~y1 = y0 + (g2 - 1)
-                ~j1 = j + primeY
-                ~b = 0.5 - x1 * x1 - y1 * y1
-             in if b <= 0
-                  then 0
-                  else
-                    (b * b)
-                      * (b * b)
-                      * gradCoord2 seed i1 j1 x1 y1
+            let x1 = x0 + g2
+                y1 = y0 + (g2 - 1)
+                j1 = j + primeY
+                b = 0.5 - x1 * x1 - y1 * y1
+             in attenuate b i j1 x1 y1
         | otherwise =
-            let ~x1 = x0 + (g2 - 1)
-                ~i1 = i + primeX
-                ~y1 = y0 + g2
-                ~j1 = j
-                ~b = 0.5 - x1 * x1 - y1 * y1
-             in if b <= 0
-                  then 0
-                  else
-                    (b * b)
-                      * (b * b)
-                      * gradCoord2 seed i1 j1 x1 y1
+            let x1 = x0 + (g2 - 1)
+                i1 = i + primeX
+                y1 = y0 + g2
+                b = 0.5 - x1 * x1 - y1 * y1
+             in attenuate b i1 j x1 y1
 
-      c =
+      !n2 =
         let g2t = 1 - 2 * g2
-         in 2 * g2t * (1 / g2 - 2) * t
-              + (-2 * g2t * g2t + a)
-      n2
-        | c <= 0 = 0
-        | otherwise =
-            let ~x2 = x0 + (2 * g2 - 1)
-                ~y2 = y0 + (2 * g2 - 1)
-             in (c * c)
-                  * (c * c)
-                  * gradCoord2 seed (i + primeX) (j + primeY) x2 y2
-   in (n0 + n1 + n2) * 99.83685446303647
+            c = 2 * g2t * (1 / g2 - 2) * t + (-2 * g2t * g2t + a)
+            x2 = x0 + (2 * g2 - 1)
+            y2 = y0 + (2 * g2 - 1)
+         in attenuate c (i + primeX) (j + primeY) x2 y2
+   in normalize $ n0 + n1 + n2
+ where
+  attenuate :: (RealFrac a) => a -> Hash -> Hash -> a -> a -> a
+  attenuate v i j x y
+    | v <= 0 = 0
+    | otherwise = (v * v) * (v * v) * gradCoord2 seed i j x y
+  {-# INLINE attenuate #-}
 {-# INLINE noise2Base #-}
+
+normalize :: (RealFrac a) => a -> a
+normalize = (99.83685446303647 *)
+{-# INLINE normalize #-}
